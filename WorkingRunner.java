d4c42253e224888f9c894776fcd02767a0bb6675
unknown <darshan.hm.xworkz@gmail.com>
2023-01-20 11:46:47 +0530

    hello

---
+++ WorkingRunner.java
@@ -0,0 +1,41 @@
+class WorkingRunner{
+
+ public static void main(String[] args){
+ 
+      int d5= Working.age("manoj",7675432189L);
+	  
+	  System.out.println("manoj age"+d5);
+ 
+     int d6= Working.age("dinkar",6747488393L);
+	  
+	  System.out.println("dinkar age"+d6);
+	  
+	  int d7= Working.age("jayanth",6546378934L);
+	  
+	  System.out.println("jayanth age"+d7);
+	  
+	  int d8= Working.age("mali",6342526171L);
+	  
+	  System.out.println("mali age"+d8);
+	  
+	  int d9= Working.age("varun",6463527282L);
+	  
+	  System.out.println("varun age"+d9);
+	  
+	  int d10= Working.age("santhu",6463527282L);
+	  
+	  System.out.println("santhu age"+d10);
+	  
+	  int d11= Working.age("nayana",6453637892L);
+	  
+	  System.out.println("nayana age"+d11);
+	  
+	  int d12= Working.age("sinchana",6453637899L);
+	  
+	  System.out.println("sinchana age"+d12);
+	  
+	  int d13= Working.age("nitya",6897654322L);
+	  
+	  System.out.println("nitya age"+d13);
+ }
+}
